52172948ddd594c6203cb8d95f7c6915341558d4
Mikhail Vlasenko <ya@smkthat.ru>
2025-07-25 21:42:47 +1100

    fix: always wrap content in a slot function

--- vue/RenderNodes.ts
+++ vue/RenderNodes.ts
@@ -37,7 +37,8 @@ export default defineComponent({
             return renderView<VNode>(
                 content,
                 serializers,
-                (tag, attrs, content) => h(tag, attrs, content as any)
+                (tag, attrs, content) => 
+                    h(tag, attrs, { default: () => content})
             );
         };
     },
